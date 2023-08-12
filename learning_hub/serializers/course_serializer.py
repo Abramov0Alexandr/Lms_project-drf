09b7cfeadab404efff9cf9c2843affe1ac664142
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-08-13 01:37:47 +0300

    Реализовал поле вывода информации об уроках

--- learning_hub/serializers/course_serializer.py
+++ learning_hub/serializers/course_serializer.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers, permissions
 from learning_hub.models import Course
+from learning_hub.serializers.lesson_seriaizer import LessonSerializer
 
 
 class CourseSerializer(serializers.ModelSerializer):
 
     lessons_count = serializers.IntegerField(source='course.all.count', read_only=True)
+    course_list = LessonSerializer(source='course', many=True)
 
     class Meta:
         model = Course
