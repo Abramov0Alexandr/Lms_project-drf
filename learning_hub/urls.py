cbda2f259db69b2083575504ae5870f7a63d6240
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-08-16 17:23:34 +0300

    Настроил маршруты для получения токена

--- learning_hub/urls.py
+++ learning_hub/urls.py
@@ -1,7 +1,8 @@
-from rest_framework import routers
 from django.urls import path
-from .views import course_views, lesson_views
+from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .apps import LearningHubConfig
+from .views import course_views, lesson_views
 
 
 app_name = LearningHubConfig.name
@@ -11,11 +12,15 @@ router.register(r'course', course_views.CourseViewSet, basename='course')
 
 urlpatterns = [
 
-    path('lesson', lesson_views.LessonListApiView.as_view(), name='lesson_list'),
+    path('lesson/', lesson_views.LessonListApiView.as_view(), name='lesson_list'),
 
     path('lesson/create', lesson_views.LessonCreateApiView.as_view(), name='lesson_create'),
     path('lesson/detail/<int:pk>', lesson_views.LessonRetrieveApiView.as_view(), name='lesson_detail'),
     path('lesson/update/<int:pk>', lesson_views.LessonUpdateApiView.as_view(), name='lesson_update'),
     path('lesson/delete/<int:pk>', lesson_views.LessonDestroyApiView.as_view(), name='lesson_delete'),
 
-] + router.urls
+    path('course/token/', course_views.CourseViewSet.as_view({'post': 'get_token'}), name='course_token_obtain_pair'),
+    path('lesson/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('lesson/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+            ] + router.urls
--- payments/urls.py
+++ payments/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .apps import PaymentsConfig
 from .views import PaymentsListApiView
 
 app_name = PaymentsConfig.name
 
 urlpatterns = [
-    path('payments', PaymentsListApiView.as_view(), name='payments'),
+    path('', PaymentsListApiView.as_view(), name='payments'),
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
