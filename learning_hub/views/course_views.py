c1afe852b2274ed4229f280d6a28e1d6c16b111a
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-08-16 17:26:08 +0300

    Написал get_token @action для получения токена

--- learning_hub/views/course_views.py
+++ learning_hub/views/course_views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from learning_hub.models import Course
 from learning_hub.serializers import CourseSerializer
 
@@ -6,3 +8,7 @@ from learning_hub.serializers import CourseSerializer
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
+
+    @action(detail=False, methods=['post'])
+    def get_token(self, request):
+        return TokenObtainPairView.as_view()(request=request._request)
