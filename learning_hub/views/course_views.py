0e5379ba5ec0db4832b6cc4118619b20c46c808b
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-08-17 18:39:25 +0300

    Убрал логику получения токена из контроллера CourseViewSet

--- learning_hub/views/course_views.py
+++ learning_hub/views/course_views.py
@@ -1,15 +1,8 @@
 from rest_framework import viewsets
-from rest_framework.decorators import action
-from rest_framework_simplejwt.views import TokenObtainPairView
 from learning_hub.models import Course
 from learning_hub.serializers import CourseSerializer
 
 
-# ToDo: Закрыть эндпоинт для данного контроллера не авторизованным пользователям
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
-
-    @action(detail=False, methods=['post'])
-    def get_token(self, request):
-        return TokenObtainPairView.as_view()(request=request._request)
