e0657c1483103de0b17eb639007215cb99bedfda
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-08-18 11:28:20 +0300

    Разделил контроллеры для модели Course на CourseViewSet и CourseDetailViewSet

--- learning_hub/urls.py
+++ learning_hub/urls.py
@@ -9,15 +9,16 @@ app_name = LearningHubConfig.name
 
 router = routers.DefaultRouter()
 router.register(r'course', course_views.CourseViewSet, basename='course')
+router.register(r'course/detail', course_views.CourseDetailViewSet, basename='course_detail')
 
 urlpatterns = [
 
     path('lesson/', lesson_views.LessonListApiView.as_view(), name='lesson_list'),
 
-    path('lesson/create', lesson_views.LessonCreateApiView.as_view(), name='lesson_create'),
-    path('lesson/detail/<int:pk>', lesson_views.LessonRetrieveApiView.as_view(), name='lesson_detail'),
-    path('lesson/update/<int:pk>', lesson_views.LessonUpdateApiView.as_view(), name='lesson_update'),
-    path('lesson/delete/<int:pk>', lesson_views.LessonDestroyApiView.as_view(), name='lesson_delete'),
+    path('lesson/create/', lesson_views.LessonCreateApiView.as_view(), name='lesson_create'),
+    path('lesson/detail/<int:pk>/', lesson_views.LessonRetrieveApiView.as_view(), name='lesson_detail'),
+    path('lesson/update/<int:pk>/', lesson_views.LessonUpdateApiView.as_view(), name='lesson_update'),
+    path('lesson/delete/<int:pk>/', lesson_views.LessonDestroyApiView.as_view(), name='lesson_delete'),
 
     path('course/token/', custom_obtain_token_views.MyObtainTokenPairView.as_view(), name='course_token_obtain_pair'),
     path('lesson/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
--- learning_hub/views/course_views.py
+++ learning_hub/views/course_views.py
@@ -1,8 +1,17 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from learning_hub.models import Course
-from learning_hub.serializers import CourseSerializer
+from learning_hub.serializers import CourseListSerializer, CourseDetailSerializer
 
 
-class CourseViewSet(viewsets.ModelViewSet):
-    serializer_class = CourseSerializer
+class CourseViewSet(mixins.ListModelMixin,
+                    mixins.CreateModelMixin,
+                    viewsets.GenericViewSet):
+    serializer_class = CourseListSerializer
     queryset = Course.objects.all()
+
+
+class CourseDetailViewSet(mixins.RetrieveModelMixin,
+                          viewsets.GenericViewSet):
+    serializer_class = CourseDetailSerializer
+    queryset = Course.objects.all()
+
