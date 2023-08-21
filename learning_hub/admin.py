03e3aabee23f778c74d114d0ec57a81de0d797d1
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-08-21 12:28:47 +0300

    Рефакторинг (добавил list_editable, изменил list_display)

--- learning_hub/admin.py
+++ learning_hub/admin.py
@@ -4,9 +4,12 @@ from .models import Course, Lesson
 
 @admin.register(Course)
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('title',)
+    list_display = ('title', 'course_owner',)
+    list_editable = ('course_owner',)
 
 
 @admin.register(Lesson)
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('title', 'course')
+    list_display = ('title', 'course', 'lesson_owner',)
+    list_editable = ('lesson_owner',)
+
