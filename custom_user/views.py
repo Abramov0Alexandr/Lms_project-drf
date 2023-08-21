de334ea70e08a4fa7def1db98f2c2444a2ccfceb
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-08-21 13:59:44 +0300

    Написал контроллер для класса CustomUser

--- custom_user/views.py
+++ custom_user/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
+from .models import CustomUser
+from .serializers import CustomUserSerializer
 
-# Create your views here.
+
+class CustomUserListCreateView(generics.ListCreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = [IsAdminUser]
+
