017e1595c5a951989f3dc4fa9bda785a6ebdc2a6
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-08-23 01:55:46 +0300

    Установил проверку прав доступа к контроллерам модели Course в зависимости от роли пользователя

--- learning_hub/views/course_views.py
+++ learning_hub/views/course_views.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from learning_hub.models import Course
 from learning_hub.serializers import CourseListSerializer, CourseDetailSerializer
+from learning_hub.permissions.course_permissions import IsCourseOwner
+from learning_hub.permissions.common_permissions import IsModerator, IsNotModerator
 
 
 class CourseViewSet(mixins.CreateModelMixin,
@@ -15,7 +18,25 @@ class CourseViewSet(mixins.CreateModelMixin,
     """
 
     serializer_class = CourseListSerializer
-    queryset = Course.objects.all()
+
+    permission_classes_by_action = {
+        'create': [IsNotModerator | IsAdminUser],
+        'update': [IsCourseOwner | IsModerator | IsAdminUser],
+        'partial_update': [IsCourseOwner | IsModerator | IsAdminUser],
+        'destroy': [IsCourseOwner | IsAdminUser],
+    }
+
+    def get_queryset(self):
+        """
+        Метод переопределен для корректного вывода информации в зависимости от роли пользователя
+        :return: список объектов в зависимости от роли авторизованного пользователя
+        """
+
+        if self.request.user.is_superuser or self.request.user.is_staff or self.request.user.groups.filter(
+                name="Модераторы").exists():
+            return Course.objects.all()
+
+        return Course.objects.filter(course_owner=self.request.user)
 
     def perform_create(self, serializer):
         """
@@ -35,3 +56,13 @@ class CourseViewSet(mixins.CreateModelMixin,
         serializer = CourseDetailSerializer(instance)
         return Response(serializer.data)
 
+    def get_permissions(self):
+        """
+        Данный метод определяет право доступа к контроллеру в зависимости от роли пользователя.
+        Если текущее действие не определено в словаре 'permission_classes_by_action',
+        он возвращает список с правом доступа только для аутентифицированных пользователей.
+        """
+
+        if self.action in self.permission_classes_by_action:
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+        return [IsAuthenticated()]
