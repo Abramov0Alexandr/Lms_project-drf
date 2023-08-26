2b7b834ac6332ffdf32cc466470a9f1975705f8d
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-08-26 22:42:42 +0300

    Написал контроллеры для подписки и отписки на курсы, прописал маршруты

---
+++ subscription/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from subscription.apps import SubscriptionConfig
+from subscription.views import SubscribeToCourse, UnsubscribeFromCourse
+
+app_name = SubscriptionConfig.name
+
+
+urlpatterns = [
+    path('subscribe/<int:course_id>/', SubscribeToCourse.as_view(), name='payments'),
+    path('unsubscribe/<int:course_id>/', UnsubscribeFromCourse.as_view(), name='payments'),
+]
--- subscription/views.py
+++ subscription/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from learning_hub.models import Course
+from subscription.models import CourseSubscription
 
-# Create your views here.
+
+class SubscribeToCourse(APIView):
+    def post(self, request, course_id):
+        course = Course.objects.get(pk=course_id)
+        subscription, created = CourseSubscription.objects.get_or_create(user=request.user, course=course)
+        subscription.is_subscribed = True
+        subscription.save()
+        return Response({'message': 'Вы успешно подписались на курс.'}, status=status.HTTP_200_OK)
+
+
+class UnsubscribeFromCourse(APIView):
+    def post(self, request, course_id):
+        course = Course.objects.get(pk=course_id)
+        subscription = CourseSubscription.objects.get(user=request.user, course=course)
+        subscription.delete()
+        return Response({'message': 'Вы успешно отписались от курса.'}, status=status.HTTP_200_OK)
